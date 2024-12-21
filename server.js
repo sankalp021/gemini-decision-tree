29b740b3e812c7a6438734ed39085a7d3b5e0f73
sankalp021 <105117044+sankalp021@users.noreply.github.com>
2024-12-22 02:07:31 +0530

    Import CORS middleware in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // Import cors
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
